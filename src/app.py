5170a58d95c1481a33882b8502a93e3d3f979b1c
Ahsan-Khan-Beatdapp <ahsan.khan@beatdapp.com>
2023-12-04 15:38:42 -0800

    changed app.py and added code to make it look better

--- src/app.py
+++ src/app.py
@@ -7,13 +7,78 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Echo To Screen">
-     </form>
-     '''
+    <html>
+        <head>
+            <title>Input Echo</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    text-align: center;
+                    padding: 50px;
+                }
+                form {
+                    background-color: white;
+                    padding: 20px;
+                    border-radius: 8px;
+                    display: inline-block;
+                }
+                input[type="text"] {
+                    padding: 10px;
+                    margin: 10px;
+                    border-radius: 4px;
+                    border: 1px solid #ddd;
+                    width: 200px;
+                }
+                input[type="submit"] {
+                    padding: 10px 20px;
+                    border-radius: 4px;
+                    border: none;
+                    background-color: #007bff;
+                    color: white;
+                    cursor: pointer;
+                }
+                input[type="submit"]:hover {
+                    background-color: #0056b3;
+                }
+            </style>
+        </head>
+        <body>
+            <form action="/echo_user_input" method="POST">
+                <input type="text" name="user_input">
+                <input type="submit" value="Echo To Screen">
+            </form>
+        </body>
+    </html>
+    '''
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    return f'''
+    <html>
+        <head>
+            <title>Input Echo</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    text-align: center;
+                    padding: 50px;
+                }
+                .echo-text {
+                    background-color: white;
+                    padding: 20px;
+                    border-radius: 8px;
+                    display: inline-block;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="echo-text">You entered: {input_text}</div>
+        </body>
+    </html>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
