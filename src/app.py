2bb22297792ff5e064ac113f3050f225816b5dc8
Ahsan-Khan-Beatdapp <ahsan.khan@beatdapp.com>
2023-12-04 15:43:35 -0800

    changed app.py again and added new code

--- src/app.py
+++ src/app.py
@@ -7,78 +7,20 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-    <html>
-        <head>
-            <title>Input Echo</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    background-color: #f4f4f4;
-                    text-align: center;
-                    padding: 50px;
-                }
-                form {
-                    background-color: white;
-                    padding: 20px;
-                    border-radius: 8px;
-                    display: inline-block;
-                }
-                input[type="text"] {
-                    padding: 10px;
-                    margin: 10px;
-                    border-radius: 4px;
-                    border: 1px solid #ddd;
-                    width: 200px;
-                }
-                input[type="submit"] {
-                    padding: 10px 20px;
-                    border-radius: 4px;
-                    border: none;
-                    background-color: #007bff;
-                    color: white;
-                    cursor: pointer;
-                }
-                input[type="submit"]:hover {
-                    background-color: #0056b3;
-                }
-            </style>
-        </head>
-        <body>
-            <form action="/echo_user_input" method="POST">
-                <input type="text" name="user_input">
-                <input type="submit" value="Echo To Screen">
-            </form>
-        </body>
-    </html>
+    <style>
+        body { background-color: #fafafa; font-family: Arial, sans-serif; }
+        form { margin: 100px auto; width: 300px; }
+        input[type="text"] { width: 100%; padding: 10px; margin: 10px 0; box-sizing: border-box; }
+        input[type="submit"] { width: 100%; padding: 10px; background-color: #4CAF50; color: white; border: none; cursor: pointer; }
+        input[type="submit"]:hover { background-color: #45a049; }
+    </style>
+    <form action="/echo_user_input" method="POST">
+        <input type="text" name="user_input" placeholder="Enter something">
+        <input type="submit" value="Echo To Screen">
+    </form>
     '''
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return f'''
-    <html>
-        <head>
-            <title>Input Echo</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    background-color: #f4f4f4;
-                    text-align: center;
-                    padding: 50px;
-                }
-                .echo-text {
-                    background-color: white;
-                    padding: 20px;
-                    border-radius: 8px;
-                    display: inline-block;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="echo-text">You entered: {input_text}</div>
-        </body>
-    </html>
-    '''
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return "You entered: " + input_text
