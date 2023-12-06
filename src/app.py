e31ceb655755f7f6c8a0a6fb1478f438b6f112c6
Ahsan-Khan-Beatdapp <ahsan.khan@beatdapp.com>
2023-12-05 20:01:20 -0800

    added tweepy

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
+# Importing
 from flask import Flask, request
+
+# keys
+api_key = '1MVFftXceUfhExRkSqSpXhy6C'
+api_secret_key = "IZb1jbpENLiRqjOVt2vgEhR3jen2CO2RIobZh7E2cACsU95vA5"
+bearer_tokem = 'AAAAAAAAAAAAAAAAAAAAAKwkbwEAAAAAI%2BLe3sh6dNEIGF9JCIL5U%2BpCCeY%3DmJgV6Opqm8Km8XNcf04llAVRvKD98SPvtWaZNwvweo09dmM834'
+access_token = "1517584070154272768-EvlYEM0uP8LUaup9GTxQqWbTulegc9"
+access_token_secret = 'BsMD78683Qb2IY5XJRv1ijRx8pxzNtenCJV83pEKvn8KO'
+
+
+# Initilizations
 
 app = Flask(__name__)
 
+# Website
 @app.route("/")
 def main():
     return '''
@@ -38,3 +50,14 @@ def echo_input():
         <p class="user-output">''' + input_text + '''</p>
     </div>
     '''
+
+
+# Tweepy data extraction
+
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
