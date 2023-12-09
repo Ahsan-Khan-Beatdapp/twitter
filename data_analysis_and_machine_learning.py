066cc113d2e9a2e5a5ed55092adb9186b655e874
Ahsan-Khan-Beatdapp <ahsan.khan@beatdapp.com>
2023-12-09 05:26:32 -0800

    Update data_analysis_and_machine_learning.py

--- data_analysis_and_machine_learning.py
+++ data_analysis_and_machine_learning.py
@@ -12,9 +12,9 @@ def calculate_average_sentiment(team_db):
     posts = [row[0] for row in cursor.fetchall()]
 
     # Initialize the sentiment analysis pipeline
-    nlp = pipeline('sentiment-analysis', model='cardiffnlp/twitter-roberta-base-sentiment')
+    nlp = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
     # Define a batch size
-    batch_size = 200
+    batch_size = 10
 
     # Perform sentiment analysis on each batch of posts and store the results
     sentiment_scores = []
@@ -29,7 +29,7 @@ def calculate_average_sentiment(team_db):
             sentiment_scores.append(score)
 
         # Introduce a delay
-        time.sleep(3)
+        #time.sleep(3)
 
     # Calculate and return the average sentiment score
     return sum(sentiment_scores) / len(sentiment_scores)
