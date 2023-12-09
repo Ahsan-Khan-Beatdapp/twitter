f41f0e5b53ec05f97b43e10ff320fd45be102e9e
Ahsan-Khan-Beatdapp <ahsan.khan@beatdapp.com>
2023-12-09 03:23:35 -0800

    made code faster

--- data_analysis_and_machine_learning.py
+++ data_analysis_and_machine_learning.py
@@ -12,16 +12,20 @@ def calculate_average_sentiment(team_db):
     # Initialize the sentiment analysis pipeline
     nlp = pipeline('sentiment-analysis')
 
-    # Perform sentiment analysis on each post and store the results
-    sentiment_scores = []
-    posts = df['post_text'].tolist()
-    results = nlp(posts, truncation=True, max_length=512, padding=True)
+    # Define a batch size
+    batch_size = 100
 
-    for result in results:
-        score = result['score'] * 100 if result['label'] == 'POSITIVE' else -(result['score'] * 100)
-        # Transform the score range from -100 to 100 to 0 to 100
-        score = (score + 100) / 2
-        sentiment_scores.append(score)
+    # Perform sentiment analysis on each batch of posts and store the results
+    sentiment_scores = []
+    for i in range(0, len(posts), batch_size):
+        batch = posts[i:i+batch_size]
+        results = nlp(batch, truncation=True, max_length=512, padding=True)
+
+        for result in results:
+            score = result['score'] * 100 if result['label'] == 'POSITIVE' else -(result['score'] * 100)
+            # Transform the score range from -100 to 100 to 0 to 100
+            score = (score + 100) / 2
+            sentiment_scores.append(score)
 
     # Calculate the average sentiment score
     if len(sentiment_scores) > 0:
