4e7204f075a88b0e716ae83121c29de64c9a7881
Ahsan-khan <145383151+Ahsan-Khan-Beatdapp@users.noreply.github.com>
2023-12-09 20:08:31 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from flask import Flask, request, redirect, url_for
 from data_analysis_and_machine_learning import calculate_average_sentiment
 from fetch_reddit_data_and_create_db import fetch_data_and_create_db
+from datetime import datetime
 #from transformers import pipeline
 
 # Load the model once when your application starts
@@ -11,36 +12,38 @@ from fetch_reddit_data_and_create_db import fetch_data_and_create_db
 
 app = Flask(__name__)
 
+@app.route("/update", methods=["POST"])
+def update_data():
+    fetch_data_and_create_db()
+    return redirect(url_for('main'))
+
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == "POST":
         team_db = request.form.get("team") + ".db"
-        return redirect(url_for('echo_input', team_db=team_db))
+        return redirect(url_for('display_mood', team_db=team_db))
     else:
         return '''
         <style>
             body { background-color: #fafafa; font-family: Arial, sans-serif; }
             form { margin: 100px auto; width: 300px; }
             select, input[type="submit"] { width: 100%; padding: 10px; margin: 10px 0; box-sizing: border-box; }
-            input[type="submit"] { background-color: #4CAF50; color: white; border: none; cursor: pointer; }
-            input[type="submit"]:hover { background-color: #45a049; }
-            h1 { text-align: center; }
         </style>
-        <h1>Ahsan's NBA Fans Mood Calculator</h1>
-        <form method="POST">
+        <form action="/" method="post">
             <select name="team">
                 <option value="lakers">Lakers</option>
-                <option value="raptors">Raptors</option>
                 <option value="celtics">Celtics</option>
+                <option value="raptors">Raptors</option>
             </select>
-            <input type="submit" value="Calculate Fan Happiness Level">
+            <input type="submit" value="Submit">
+        </form>
+        <form action="/update" method="post">
+            <input type="submit" value="Update Data">
         </form>
         '''
 
-from datetime import datetime
-
-@app.route("/echo_user_input")
-def echo_input():
+@app.route("/fan-happiness-level")
+def display_mood():
     team_db = request.args.get('team_db', "")
     team_name = team_db.split('.')[0]  # Extract team name from the database name
 
